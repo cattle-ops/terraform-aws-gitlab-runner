a3f6e40cd5e4f33b288e40e74125a4201c6eef6b
Niek Palm <dev.npalm@gmail.com>
2019-03-29 12:05:21 +0100

    Add exampl to show backwards compatibility

---
+++ examples/runner-pre-registered/providers.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "2.2"
+}
+
+provider "template" {
+  version = "2.1"
+}
+
+provider "local" {
+  version = "1.1"
+}
+
+provider "null" {
+  version = "2.1"
+}
+
+provider "tls" {
+  version = "1.2"
+}
