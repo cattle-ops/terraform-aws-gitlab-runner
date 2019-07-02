d0a764ac1e4265947f60a3323fb56b914b0f4fc9
Roman Pertl <533172+roock@users.noreply.github.com>
2019-07-02 11:12:45 +0200

    ci/bin/verify-examples: make check for examples working and not print some obscure error messages

--- ci/bin/verify-examples.sh
+++ ci/bin/verify-examples.sh
@@ -4,7 +4,8 @@ DIR=${1:-examples}
 source $(dirname $0)/terraform.sh
 
 EXAMPLES="$(find ${DIR} -maxdepth 1 -mindepth 1 -type d 2> /dev/null )"
-if [[ -z $EXAMPLES || "$($(echo $EXAMPLES) | wc -l)" -gt 0  ]] ; then
+echo $EXAMPLES
+if [[ -z $EXAMPLES || $( echo $EXAMPLES | wc -l )  -eq 0  ]] ; then
   echo "No example(s) directories found."
   exit 1
 fi
