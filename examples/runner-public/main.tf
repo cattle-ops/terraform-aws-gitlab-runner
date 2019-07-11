f38cba813db4347a49470133f172d80a460793fa
Niek Palm <dev.npalm@gmail.com>
2019-07-11 17:03:30 +0200

    Fix broken public example

--- examples/runner-public/main.tf
+++ examples/runner-public/main.tf
@@ -31,7 +31,6 @@ module "runner" {
   vpc_id                   = "${module.vpc.vpc_id}"
   subnet_ids_gitlab_runner = "${module.vpc.public_subnets}"
   subnet_id_runners        = "${element(module.vpc.public_subnets, 0)}"
-  aws_zone                 = "b"
 
   docker_machine_spot_price_bid = "0.1"
 
