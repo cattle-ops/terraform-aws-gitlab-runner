6f86899e2ce022cbc923d2826ff36571c8476de4
Niek Palm <dev.npalm@gmail.com>
2019-07-14 14:02:05 +0200

    #91 - Fix migration script

--- migrations/migration-state-3.7.x.sh
+++ migrations/migration-state-3.7.x.sh
@@ -4,4 +4,4 @@ echo ---
 echo --- Migration state for updates in Release 3.7.0
 echo ---
 terraform state mv module.runner.aws_s3_bucket.build_cache module.runner.module.cache.aws_s3_bucket.build_cache
-terraform state mv module.runner.aws_iam_policy.docker_machine_cache module.runner.module.cache.aws_iam_policy.do
+terraform state mv module.runner.aws_iam_policy.docker_machine_cache module.runner.module.cache.aws_iam_policy.docker_machine_cache
