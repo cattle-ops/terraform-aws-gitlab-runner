3d3dac68f165f3cd3204c21b5873c1191eb419d0
Niek Palm <dev.npalm@gmail.com>
2019-07-16 22:33:45 +0200

    Updates docs

--- variables.tf
+++ variables.tf
@@ -374,7 +374,8 @@ variable "overrides" {
 }
 
 variable "cache_bucket" {
-  description = "Create a bucket for the cache, to share cache cross multiple runners disable the creation and inject the bucket and policy for the chache."
+  description = "Configuration to control the creation of th the cache bucket. By default the bucket will be crated and used as shared cache. To use the same cache cross multiple runners disable the cration of the cache and provice a policy and bucket name. See the public runner example for more details."
+  type        = "map"
 
   default = {
     create = true
