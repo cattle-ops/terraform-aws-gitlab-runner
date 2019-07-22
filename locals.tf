334e7039e08f3514ffbba65491665c762ed7e3d9
Yurii Polishchuk <yuriy.pol@gmail.com>
2019-07-22 13:47:13 +0300

    move locals to separate file to fix formatting

---
+++ locals.tf
@@ -0,0 +1,20 @@
+locals {
+  // Convert list to a string separated and prepend by a comma
+  docker_machine_options_string = format(
+    ",%s",
+    join(",", formatlist("%q", var.docker_machine_options)),
+  )
+
+  // Ensure off peak is optional
+  runners_off_peak_periods_string = var.runners_off_peak_periods == "" ? "" : format("OffPeakPeriods = %s", var.runners_off_peak_periods)
+
+  // Define key for runner token for SSM
+  secure_parameter_store_runner_token_key = "${var.environment}-${var.secure_parameter_store_runner_token_key}"
+
+  // custom names for instances and security groups
+  name_runner_instance       = var.overrides["name_runner_agent_instance"] == "" ? local.tags["Name"] : var.overrides["name_runner_agent_instance"]
+  name_sg                    = var.overrides["name_sg"] == "" ? local.tags["Name"] : var.overrides["name_sg"]
+  runners_additional_volumes = <<EOF
+  %{~for volume in var.runners_additional_volumes~},"${volume}"%{endfor~}
+EOF
+}
