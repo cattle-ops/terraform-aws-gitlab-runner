c6f91a649fcbebf434420328f9b8b23cac73c2a5
Méril Pilon <meril.pilon@gmail.com>
2019-09-04 12:32:27 +0100

    Bump gitlab-runner to 12.2.0

--- variables.tf
+++ variables.tf
@@ -294,7 +294,7 @@ variable "cache_shared" {
 variable "gitlab_runner_version" {
   description = "Version of the GitLab runner."
   type        = string
-  default     = "12.1.0"
+  default     = "12.2.0"
 }
 
 variable "enable_gitlab_runner_ssh_access" {
@@ -365,7 +365,6 @@ variable "ami_owners" {
   type        = list(string)
   default     = ["amazon"]
 }
-
 
 variable "runner_ami_filter" {
   description = "List of maps used to create the AMI filter for the Gitlab runner docker-machine AMI."
