da74a01bea45eff74c3d9d9ce2029ccff78143f6
Arthur Diniz <arthurbdiniz@gmail.com>
2020-02-13 19:07:15 +1100

    Adds terraform 0.12 support
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- logging.tf
+++ logging.tf
@@ -1,18 +1,17 @@
 data "template_file" "instance_profile" {
-  count    = "${var.enable_cloudwatch_logging ? 1 : 0}"
-  template = "${file("${path.module}/policies/instance-logging-policy.json")}"
+  count    = var.enable_cloudwatch_logging ? 1 : 0
+  template = file("${path.module}/policies/instance-logging-policy.json")
 }
 
 resource "aws_iam_role_policy" "instance" {
-  count  = "${var.enable_cloudwatch_logging ? 1 : 0}"
+  count  = var.enable_cloudwatch_logging ? 1 : 0
   name   = "${var.environment}-instance-role"
-  role   = "${aws_iam_role.instance.name}"
-  policy = "${data.template_file.instance_profile.rendered}"
+  role   = aws_iam_role.instance.name
+  policy = data.template_file.instance_profile[count.index].rendered
 }
 
 resource "aws_cloudwatch_log_group" "environment" {
-  count = "${var.enable_cloudwatch_logging ? 1 : 0}"
-  name  = "${var.environment}"
-
-  tags = "${local.tags}"
+  count = var.enable_cloudwatch_logging ? 1 : 0
+  name  = var.environment
+  tags  = local.tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "runner_as_group_name" {
   description = "Name of the autoscaling group for the gitlab-runner instance"
-  value       = "${aws_autoscaling_group.gitlab_runner_instance.name}"
+  value       = aws_autoscaling_group.gitlab_runner_instance.name
 }
 
 output "runner_cache_bucket_arn" {
   description = "ARN of the S3 for the build cache."
-  value       = "${aws_s3_bucket.build_cache.arn}"
+  value       = aws_s3_bucket.build_cache.arn
 }
 
 output "runner_agent_role" {
   description = "ARN of the rule used for the ec2 instance for the GitLab runner agent."
-  value       = "${aws_iam_role.instance.arn}"
+  value       = aws_iam_role.instance.arn
 }
 
 output "runner_role" {
   description = "ARN of the rule used for the docker machine runners."
-  value       = "${aws_iam_role.docker_machine.*.arn}"
+  value       = aws_iam_role.docker_machine.*.arn
 }
