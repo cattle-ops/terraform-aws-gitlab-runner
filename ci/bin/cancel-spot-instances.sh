6bb19313b5e95fa9f2d74884b774d3a03d2f9f92
Niek Palm <dev.npalm@gmail.com>
2020-03-09 19:06:17 +0100

    Fix cancel spot instance script

--- ci/bin/cancel-spot-instances.sh
+++ ci/bin/cancel-spot-instances.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
-hash jq aws &>/dev/null || { echo >&2 "I require jq and AWS CLI but one or both is not installed.  Aborting."; exit 1; }
+hash jq aws &>/dev/null || {
+    echo >&2 "I require jq and AWS CLI but one or both is not installed.  Aborting."
+    exit 1
+}
 
 SPOT_REQUESTS=$(aws ec2 describe-spot-instance-requests --filters "Name=state,Values=active,open" | jq -r '[ .SpotInstanceRequests[] | select( .LaunchSpecification.IamInstanceProfile.Name | contains("'$1'")) ]')
 
 # It's possible there's no spot requests to cancel, so be safe.
-if ["$SPOT_REQUESTS" != "[]"]
+if ["$SPOT_REQUESTS" != "[]"]; then
     echo $SPOT_REQUESTS | jq -r '.[].InstanceId' | xargs aws ec2 terminate-instances --instance-ids
     echo $SPOT_REQUESTS | jq -r '.[].SpotInstanceRequestId' | xargs aws ec2 cancel-spot-instance-requests --spot-instance-request-ids
 else
