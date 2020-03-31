5c2eb9c3fcc1231802b9f64826fa35a1afd3ffa5
Niek Palm <dev.npalm@gmail.com>
2020-03-31 22:46:13 +0200

    #206 - Fix missing policy for existing cache

--- main.tf
+++ main.tf
@@ -451,7 +451,7 @@ resource "aws_iam_role_policy_attachment" "instance_session_manager_aws_managed"
 ### Policy for the docker machine instance to access cache
 ################################################################################
 resource "aws_iam_role_policy_attachment" "docker_machine_cache_instance" {
-  count      = var.cache_bucket["create"] ? 1 : 0
+  count      = var.cache_bucket["create"] || var.cache_bucket["policy"] != "" ? 1 : 0
   role       = aws_iam_role.instance.name
   policy_arn = local.bucket_policy
 }
