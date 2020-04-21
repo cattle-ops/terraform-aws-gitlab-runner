ae29bd7b80864035abfbbaf0ab1dc22689bb3fe2
Luis Fernando Granados <luisfer1g@gmail.com>
2020-04-21 03:22:37 -0500

    Resolve issue https://github.com/npalm/terraform-aws-gitlab-runner/issues/218 (#219)
    Co-authored-by: Niek Palm <dev.npalm@gmail.com>
    Co-authored-by: frcho <manager@uva3.com>

--- ci/bin/cancel-spot-instances.sh
+++ ci/bin/cancel-spot-instances.sh
@@ -8,7 +8,7 @@ hash jq aws &>/dev/null || {
 SPOT_REQUESTS=$(aws ec2 describe-spot-instance-requests --filters "Name=state,Values=active,open" | jq -r '[ .SpotInstanceRequests[] | select( .LaunchSpecification.IamInstanceProfile.Name | contains("'$1'")) ]')
 
 # It's possible there's no spot requests to cancel, so be safe.
-if ["$SPOT_REQUESTS" != "[]"]; then
+if [ "${SPOT_REQUESTS}" != "[]" ]; then
     echo $SPOT_REQUESTS | jq -r '.[].InstanceId' | xargs aws ec2 terminate-instances --instance-ids
     echo $SPOT_REQUESTS | jq -r '.[].SpotInstanceRequestId' | xargs aws ec2 cancel-spot-instance-requests --spot-instance-request-ids
 else
