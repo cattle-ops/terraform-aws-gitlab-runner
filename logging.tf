e0c1d8cefd71f62d76f8ab4ff65227fd42b52e01
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2020-06-12 16:46:56 +1000

    update with new version

--- logging.tf
+++ logging.tf
@@ -1,19 +1,7 @@
-data "template_file" "instance_profile" {
-  count    = var.enable_cloudwatch_logging ? 1 : 0
-  template = file("${path.module}/policies/instance-logging-policy.json")
-}
-
 resource "aws_iam_role_policy" "instance" {
   count  = var.enable_cloudwatch_logging ? 1 : 0
   name   = "${var.environment}-instance-role"
   role   = aws_iam_role.instance.name
-  policy = data.template_file.instance_profile[count.index].rendered
-}
-
-resource "aws_cloudwatch_log_group" "environment" {
-  count  = var.enable_cloudwatch_logging ? 1 : 0
-  name   = var.environment
-  tags   = local.tags
   policy = templatefile("${path.module}/policies/instance-logging-policy.json", { arn_format = var.arn_format })
 }
 
