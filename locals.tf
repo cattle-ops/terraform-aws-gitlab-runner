60fa02206ed701783773f6489207b6ed7ac229ed
Matthias Kay <matthias.kay@it-consulting-rosengarten.de>
2020-07-28 23:03:20 +0200

    Format issue

--- locals.tf
+++ locals.tf
@@ -6,10 +6,15 @@ locals {
   )
 
   // convert the options for the session server
-  session_server_string = join("", formatlist("%s", ["[session_server]",
-                                            format("listen_address = %q", var.session_server_listen_address),
-                                            format("advertise_address = %q", var.session_server_advertise_address),
-                                            format("session_timeout = %s", var.session_server_session_timeout)]))
+  session_server_string = join("",
+    formatlist("%s", [
+      "[session_server]",
+      format("listen_address = %q", var.session_server_listen_address),
+      format("advertise_address = %q", var.session_server_advertise_address),
+      format("session_timeout = %s", var.session_server_session_timeout)
+      ]
+    )
+  )
 
   // Ensure off peak is optional
   runners_off_peak_periods_string = var.runners_off_peak_periods == "" ? "" : format("OffPeakPeriods = %s", var.runners_off_peak_periods)
