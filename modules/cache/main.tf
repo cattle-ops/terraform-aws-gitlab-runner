212ce2060ad02961f57327c9086afa27e6cfca28
Flip <flip@fliphess.com>
2020-09-01 19:45:04 +0200

    Use format() for both string formattings

--- modules/cache/main.tf
+++ modules/cache/main.tf
@@ -12,8 +12,8 @@ locals {
     var.tags,
   )
 
-  cache_bucket_prefix = var.cache_bucket_name_include_account_id ? "${var.cache_bucket_prefix}${data.aws_caller_identity.current.account_id}-gitlab-runner-cache" : "${var.cache_bucket_prefix}-gitlab-runner-cache"
-  cache_bucket_name   = var.cache_bucket_set_suffix ? format("%s-%s", local.cache_bucket_prefix, random_string.s3_suffix.result) : local.cache_bucket_prefix
+  cache_bucket_string = var.cache_bucket_name_include_account_id ? format("%s%s-gitlab-runner-cache", var.cache_bucket_prefix, data.aws_caller_identity.current.account_id) : format("%s-gitlab-runner-cache", var.cache_bucket_prefix)
+  cache_bucket_name   = var.cache_bucket_set_suffix ? format("%s-%s", local.cache_bucket_string, random_string.s3_suffix.result) : local.cache_bucket_string
 }
 
 resource "random_string" "s3_suffix" {
