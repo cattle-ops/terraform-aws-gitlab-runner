16e6ed15c32b007745ffc5992d9dcc5f9373f670
Niek Palm <npalm@users.noreply.github.com>
2021-05-07 13:40:34 +0200

    Add tags to aws_iam_policy

--- versions.tf
+++ versions.tf
@@ -3,7 +3,8 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      version = ">= 3.35.0"
+      source  = "hashicorp/aws"
     }
     null = {
       source = "hashicorp/null"
