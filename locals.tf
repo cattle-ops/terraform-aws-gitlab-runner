c9c4c44fc1b7df694e2bdf802299e7a25441e9e3
Matthias Kay <github@matthiaskay.de>
2021-10-19 23:25:25 +0200

    fix: Ensure the existence of overrides["name_iam_objects"] before accessing
    Co-authored-by: kayma <kayma@hlag.com>

--- locals.tf
+++ locals.tf
@@ -15,7 +15,7 @@ locals {
   // custom names for instances and security groups
   name_runner_agent_instance = var.overrides["name_runner_agent_instance"] == "" ? local.tags["Name"] : var.overrides["name_runner_agent_instance"]
   name_sg                    = var.overrides["name_sg"] == "" ? local.tags["Name"] : var.overrides["name_sg"]
-  name_iam_objects           = var.overrides["name_iam_objects"] == "" ? local.tags["Name"] : var.overrides["name_iam_objects"]
+  name_iam_objects           = lookup(var.overrides, "name_iam_objects", "") == "" ? local.tags["Name"] : var.overrides["name_iam_objects"]
   runners_additional_volumes = <<-EOT
   %{~for volume in var.runners_additional_volumes~},"${volume}"%{endfor~}
   EOT
