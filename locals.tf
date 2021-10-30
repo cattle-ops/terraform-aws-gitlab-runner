f95b6d22c63ef73aba2190010049c1c292eac4c4
kayma <kayma@hlag.com>
2021-10-30 14:24:31 +0200

    Add /builds to the volume mount to have everything available inside docker

--- locals.tf
+++ locals.tf
@@ -17,7 +17,7 @@ locals {
   name_sg                    = var.overrides["name_sg"] == "" ? local.tags["Name"] : var.overrides["name_sg"]
   name_iam_objects           = var.overrides["name_iam_objects"] == "" ? local.tags["Name"] : var.overrides["name_iam_objects"]
   runners_additional_volumes = <<-EOT
-  %{~if var.runners_add_dind_volumes~},"/certs/client", "/var/run/docker.sock:/var/run/docker.sock"%{endif~}%{~for volume in var.runners_additional_volumes~},"${volume}"%{endfor~}
+  %{~if var.runners_add_dind_volumes~},"/certs/client", "/builds", "/var/run/docker.sock:/var/run/docker.sock"%{endif~}%{~for volume in var.runners_additional_volumes~},"${volume}"%{endfor~}
   EOT
 
   runners_machine_autoscaling = templatefile("${path.module}/template/runners_machine_autoscaling.tpl", {
