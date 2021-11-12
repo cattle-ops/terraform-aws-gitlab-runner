3b0552b724e59c36ad00b3fac5b8cb33ffd6ab37
kayma <kayma@hlag.com>
2021-11-12 15:49:23 +0100

    Always read the token

--- main.tf
+++ main.tf
@@ -21,13 +21,20 @@ resource "aws_ssm_parameter" "runner_registration_token" {
   }
 }
 
+# to read the current token for the null_resource. aws_ssm_parameter.runner_registration_token.value is never updated!
+data "aws_ssm_parameter" "current_runner_registration_token" {
+  depends_on = [aws_ssm_parameter.runner_registration_token]
+
+  name = local.secure_parameter_store_runner_token_key
+}
+
 resource "null_resource" "remove_runner" {
   depends_on = [aws_ssm_parameter.runner_registration_token]
 
   triggers = {
     aws_region                = var.aws_region
     runners_gitlab_url        = var.runners_gitlab_url
-    runner_registration_token = aws_ssm_parameter.runner_registration_token.value
+    runner_registration_token = data.aws_ssm_parameter.current_runner_registration_token.value
   }
 
   provisioner "local-exec" {
