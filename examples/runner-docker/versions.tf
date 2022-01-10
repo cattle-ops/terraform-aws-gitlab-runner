ce54abc6dbc73e52ba249c1c0c149cc129999418
Matthias Kay <github@matthiaskay.de>
2022-01-10 20:33:54 +0100

    Add configuration_aliases to examples

--- examples/runner-docker/versions.tf
+++ examples/runner-docker/versions.tf
@@ -1,10 +1,15 @@
-
+/* Providers used:
+     aws:              the default provider which is also used to create the Gitlab Runner
+     aws.cache_bucket: a provider to create the S3 cache bucket in another region.
+*/
 terraform {
   required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.35"
+
+      configuration_aliases = [aws.cache_bucket]
     }
     local = {
       source  = "hashicorp/local"
