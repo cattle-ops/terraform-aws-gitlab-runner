98fd7714331fbfa2107bf00af0f46dbfecaba271
Mustafa Abdul-Kader <mustafa@muszr.me>
2022-02-15 13:19:47 -0600

    pin version >= 4.0

--- modules/cache/versions.tf
+++ modules/cache/versions.tf
@@ -1,4 +1,11 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      version = ">= 4.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
