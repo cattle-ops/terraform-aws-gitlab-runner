3e7d62a9b90c2ec34dc30582599fe218157fe2a3
Gábor Boros <gabor.brs@gmail.com>
2022-04-06 14:32:25 +0200

    fix: bump gitlab-runner version
    It seems that the current gitlab-runner version is not available anymore in the yum repo.

--- variables.tf
+++ variables.tf
@@ -361,7 +361,7 @@ variable "cache_shared" {
 variable "gitlab_runner_version" {
   description = "Version of the [GitLab runner](https://gitlab.com/gitlab-org/gitlab-runner/-/releases)."
   type        = string
-  default     = "14.8.2"
+  default     = "14.9.1"
 }
 
 variable "enable_ping" {
