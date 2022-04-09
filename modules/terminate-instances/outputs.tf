0b0626a16ded0cc5d307f26ec60ba4a1f38517f6
Niek Palm <dev.npalm@gmail.com>
2022-04-09 17:37:44 +0200

    chore: Auto generate terraform docs

--- modules/terminate-instances/outputs.tf
+++ modules/terminate-instances/outputs.tf
@@ -4,20 +4,20 @@
 
 output "lambda_function_arn" {
   description = "Lambda function arn."
-  value = aws_lambda_function.terminate_runner_instances.arn
+  value       = aws_lambda_function.terminate_runner_instances.arn
 }
 
 output "lambda_function_invoke_arn" {
   description = "Lambda function invoke arn."
-  value = aws_lambda_function.terminate_runner_instances.invoke_arn
+  value       = aws_lambda_function.terminate_runner_instances.invoke_arn
 }
 
 output "lambda_function_name" {
   description = "Lambda function name."
-  value = aws_lambda_function.terminate_runner_instances.function_name
+  value       = aws_lambda_function.terminate_runner_instances.function_name
 }
 
 output "lambda_function_source_code_hash" {
   description = "Lambda function source code hash."
-  value = aws_lambda_function.terminate_runner_instances.source_code_hash
+  value       = aws_lambda_function.terminate_runner_instances.source_code_hash
 }
