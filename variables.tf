77601bd21e15e78a30c25fa4c46dfe30768078e6
Jon McEwen <jon.mcewen@naimuri.com>
2022-05-23 09:33:34 +0100

    add description to new variable

--- variables.tf
+++ variables.tf
@@ -671,6 +671,7 @@ variable "runners_services_volumes_tmpfs" {
 }
 
 variable "runners_docker_services" {
+  description = "adds `runners.docker.services` blocks to config.toml.  All fields must be set (examine the Dockerfile of the service image for the entrypoint - see ./examples/runner-default/main.tf)"
   type = list(object({
     name       = string
     alias      = string
