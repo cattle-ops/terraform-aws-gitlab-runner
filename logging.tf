78fbf2c7b457147b8c92281d200433b4511acaee
Bjorn Olsen <bjorn@cloudandthings.io>
2022-11-23 14:03:09 +0200

    Allow custom runner agent IAM role fixups (#572)

--- logging.tf
+++ logging.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "instance" {
-  count  = var.enable_cloudwatch_logging ? 1 : 0
-  name   = "${local.name_iam_objects}-instance-role"
+  count  = var.enable_cloudwatch_logging && var.create_runner_iam_role ? 1 : 0
+  name   = "${local.name_iam_objects}-logging"
   role   = local.aws_iam_role_instance_name
   policy = templatefile("${path.module}/policies/instance-logging-policy.json", { partition = data.aws_partition.current.partition })
 }
