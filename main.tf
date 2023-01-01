b9e73fe212cabb7d60d6d2ceda5e71c4369dab51
r33drichards <rwendt1337@gmail.com>
2023-01-01 05:22:34 -0800

    fix: Invalid Function Arguement when passing bucket as arg (#266)
    * fix Invalid Function Arguement when passing bucket as arg
    
    see https://github.com/npalm/terraform-aws-gitlab-runner/issues/265
    
    * docs: detail the usage of S3 cache policy (#427)
    
    * Add remarks to explain traps in the code
    
    * Format code
    
    * leave the comment, no code changes
    
    Co-authored-by: Robert <2912934-reedrichards@users.noreply.gitlab.com>
    Co-authored-by: Matthias Kay <github-public@matthiaskay.de>

--- main.tf
+++ main.tf
@@ -303,8 +303,8 @@ resource "aws_launch_template" "gitlab_runner_instance" {
 ### Create cache bucket
 ################################################################################
 locals {
-  bucket_name   = var.cache_bucket["create"] ? module.cache[0].bucket : lookup(var.cache_bucket, "bucket", "")
-  bucket_policy = var.cache_bucket["create"] ? module.cache[0].policy_arn : lookup(var.cache_bucket, "policy", "")
+  bucket_name   = var.cache_bucket["create"] ? module.cache[0].bucket : var.cache_bucket["bucket"]
+  bucket_policy = var.cache_bucket["create"] ? module.cache[0].policy_arn : var.cache_bucket["policy"]
 }
 
 module "cache" {
