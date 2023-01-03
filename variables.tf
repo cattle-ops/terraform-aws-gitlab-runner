45f0673ede8fabf14caa3b499bba672b1dffaf7a
Matthias Kay <github-public@matthiaskay.de>
2023-01-03 11:32:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -622,7 +622,7 @@ variable "overrides" {
   
   validation {
     condition = var.overrides["name_docker_machine_runners"] != "" && can(regex("^[a-zA-Z0-9\\.-]+$", var.overrides["name_docker_machine_runners"]))
-    error_message = "Valid characters for the docker machine name are: [a-zA-Z0-9\\.-]"
+    error_message = "Valid characters for the docker machine name are: [a-zA-Z0-9\\.-]."
   }
 }
 
