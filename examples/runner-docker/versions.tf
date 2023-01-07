325781f0c6b5788b0647b9a8c32acb98b16ce4d1
Matthias Kay <github-public@matthiaskay.de>
2023-01-07 13:16:53 +0100

    format code

--- examples/runner-docker/versions.tf
+++ examples/runner-docker/versions.tf
@@ -3,23 +3,23 @@ terraform {
   required_version = ">= 1"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.49.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.2.3"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.1"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "4.0.4"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.4.3"
     }
   }
