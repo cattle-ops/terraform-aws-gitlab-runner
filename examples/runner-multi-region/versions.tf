974c7adceb49e260e8089dfcffc67f3f19d60b1e
Matthias Kay <github-public@matthiaskay.de>
2023-01-07 13:07:20 +0100

    Update versions.tf

--- examples/runner-multi-region/versions.tf
+++ examples/runner-multi-region/versions.tf
@@ -4,23 +4,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.7"
+      version = ">= 4.7"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2"
+      version = ">= 2"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3"
+      version = ">= 3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
 }
