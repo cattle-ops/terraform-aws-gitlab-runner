29319b49c242cf27ce7dec66c3e69184d80bda6a
Matthias Kay <github-public@matthiaskay.de>
2023-01-07 14:14:14 +0100

    add s3 endpoint

--- examples/runner-default/main.tf
+++ examples/runner-default/main.tf
@@ -20,7 +20,24 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
-  enable_s3_endpoint = true
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "3.18.1"
+
+  vpc_id             = module.vpc.vpc_id
+
+  endpoints = {
+    s3 = {
+      service = "s3"
+      tags    = { Name = "s3-vpc-endpoint" }
+    }
+  }
 
   tags = {
     Environment = var.environment
