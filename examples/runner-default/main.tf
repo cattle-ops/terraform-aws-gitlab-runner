703d0a530277acad0efcefce0fc3051c6597561b
Matthias Kay <github-public@matthiaskay.de>
2023-01-07 14:17:32 +0100

    format

--- examples/runner-default/main.tf
+++ examples/runner-default/main.tf
@@ -30,7 +30,7 @@ module "vpc_endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version = "3.18.1"
 
-  vpc_id             = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   endpoints = {
     s3 = {
