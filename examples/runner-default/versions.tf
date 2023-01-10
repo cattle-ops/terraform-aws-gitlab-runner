fe291e2987c3efd53c521670ac63eccf92aaa1e9
Matthias Kay <github-public@matthiaskay.de>
2023-01-10 23:54:29 +0100

    chore: pin Terraform provider versions with lock file (#650)

--- examples/runner-default/versions.tf
+++ examples/runner-default/versions.tf
@@ -4,23 +4,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.7"
+      version = "4.49.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2"
+      version = "2.2.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = "3.2.1"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3"
+      version = "4.0.4"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "3.4.3"
     }
   }
 }
