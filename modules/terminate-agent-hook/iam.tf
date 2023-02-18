c6d68b913a998b7689629414fe4aee93c72c2ac8
kayma <kayma@hlag.com>
2023-02-18 10:40:22 +0100

    duplicate policy removed

--- modules/terminate-agent-hook/iam.tf
+++ modules/terminate-agent-hook/iam.tf
@@ -62,22 +62,6 @@ data "aws_iam_policy_document" "lambda" {
     effect = "Allow"
   }
 
-  statement {
-    sid = "SSHKeyHousekeeping"
-
-    effect = "Allow"
-    actions = [
-      "ec2:DescribeKeyPairs",
-      "ec2:DeleteKeyPair"
-    ]
-    resources = ["*"]
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:ec2:${data.aws_region.this.id}:${data.aws_caller_identity.this.account_id}:key-pair/runner-*"]
-    }
-  }
-
   # Permit the function to execute the ASG lifecycle action
   statement {
     sid    = "GitLabRunnerLifecycleTerminateEvent"
@@ -87,6 +71,7 @@ data "aws_iam_policy_document" "lambda" {
     ]
     resources = [var.asg_arn]
   }
+
   statement {
     sid = "GitLabRunnerLifecycleTerminateLogs"
     actions = [
@@ -101,9 +86,7 @@ data "aws_iam_policy_document" "lambda" {
       "${aws_cloudwatch_log_group.lambda.arn}:log-stream:*"
     ]
   }
-}
 
-data "aws_iam_policy_document" "ssh_key_housekeeping" {
   statement {
     sid = "SSHKeyHousekeepingList"
 
@@ -142,17 +125,4 @@ resource "aws_iam_policy" "lambda" {
 resource "aws_iam_role_policy_attachment" "lambda" {
   role       = aws_iam_role.lambda.name
   policy_arn = aws_iam_policy.lambda.arn
-}
-
-resource "aws_iam_policy" "ssh_key_housekeeping" {
-  name   = "${var.name_iam_objects}-${var.name}-ssh-key"
-  path   = "/"
-  policy = data.aws_iam_policy_document.ssh_key_housekeeping.json
-
-  tags = var.tags
-}
-
-resource "aws_iam_role_policy_attachment" "ssh_key_housekeeping" {
-  role       = aws_iam_role.lambda.name
-  policy_arn = aws_iam_policy.ssh_key_housekeeping.arn
 }
