9ff9f8778fd43a056a6a824aaace71dd0260aebb
Matthias Kay <github-public@matthiaskay.de>
2023-03-02 16:24:52 +0100

    undo variable deletion to avoid breaking change

--- variables.tf
+++ variables.tf
@@ -591,6 +591,12 @@ variable "gitlab_runner_registration_config" {
   }
 }
 
+variable "secure_parameter_store_runner_token_key" {
+  description = "(Deprecated and not used) The key name used store the Gitlab runner token in Secure Parameter Store"
+  type        = string
+  default     = "runner-token"
+}
+
 variable "secure_parameter_store_runner_sentry_dsn" {
   description = "The Sentry DSN name used to store the Sentry DSN in Secure Parameter Store"
   type        = string
