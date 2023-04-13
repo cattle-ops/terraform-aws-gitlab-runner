87ffa5a08a987929bc531a5c57c323abc1a47fb6
kayma <kayma@hlag.com>
2023-04-13 15:48:53 +0200

    fix python

--- modules/terminate-agent-hook/lambda/lambda_function.py
+++ modules/terminate-agent-hook/lambda/lambda_function.py
@@ -10,7 +10,7 @@ https://github.com/cattle-ops/terraform-aws-gitlab-runner/issues/317 has some di
 This is rudimentary and doesn't check if a build runner has a current job.
 """
 import boto3
-import {ClientError} from botocore.exceptions
+from botocore.exceptions import {ClientError}
 import json
 import os
 
