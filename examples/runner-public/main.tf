c8a3b89c46f749214461bade8e1e6d161d0ef860
Matthias Kay <matthias.kay@hlag.com>
2023-05-22 20:42:31 +0200

    refactor!: group variables for better overview (#810)
    Groups variables into objects to
    - reduce the number of variables (currently 118)
    - to gain a better overview of all configuration settings
    
    Creates new groups of variables:
    - `runner_manager` in case it configures the "main" process which sets
    the defaults for all runners
    - `runner` in case it configures the runner created by the runner
    manager
    - `runner_worker` in case it configures the docker/docker+machine or
    leave it as it is, if it is a global scope, e.g. common tags, the
    environment, ...
    
    Yes and a script is provided to do that. It covers 98% of all migrations
    (see migrations/migrate-to-7-0-0.sh)
    
    Please mention the examples you have verified.
    
    ---------
    
    Co-authored-by: Tyrone Meijn <tyrone_meijn@hotmail.com>

--- examples/runner-public/main.tf
+++ examples/runner-public/main.tf
@@ -29,26 +29,22 @@ module "cache" {
 module "runner" {
   source = "../../"
 
-  aws_region  = var.aws_region
+  vpc_id      = module.vpc.vpc_id
+  subnet_id   = element(module.vpc.public_subnets, 0)
   environment = var.environment
 
-  runners_use_private_address = false
+  security_group_prefix = "my-security-group"
 
-  vpc_id    = module.vpc.vpc_id
-  subnet_id = element(module.vpc.public_subnets, 0)
-
-  docker_machine_spot_price_bid = "on-demand-price"
-
-  runners_name             = var.runner_name
-  runners_gitlab_url       = var.gitlab_url
-  runners_environment_vars = ["KEY=Value", "FOO=bar"]
-
-  runners_docker_options = {
-    privileged = "false"
-    volumes    = ["/var/run/docker.sock:/var/run/docker.sock"]
+  runner_instance = {
+    name        = var.runner_name
+    name_prefix = "my-runner-agent"
   }
 
-  gitlab_runner_registration_config = {
+  runner_gitlab = {
+    url = var.gitlab_url
+  }
+
+  runner_gitlab_registration_config = {
     registration_token = var.registration_token
     tag_list           = "docker_spot_runner"
     description        = "runner public - auto"
@@ -58,38 +54,44 @@ module "runner" {
     access_level       = "ref_protected"
   }
 
-  overrides = {
-    name_sg                     = "my-security-group"
-    name_runner_agent_instance  = "my-runner-agent"
-    name_docker_machine_runners = "my-runners-dm"
+  runner_worker = {
+    environment_variables = ["KEY=Value", "FOO=bar"]
   }
 
-  cache_shared = "true"
-
-  cache_bucket = {
+  runner_worker_cache = {
+    shared = "true"
     create = false
     policy = module.cache.policy_arn
     bucket = module.cache.bucket
+  }
+
+  runner_worker_docker_options = {
+    privileged = "false"
+    volumes    = ["/cache", "/var/run/docker.sock:/var/run/docker.sock"]
+  }
+
+  runner_worker_docker_machine_instance = {
+    private_address_only = false
+    name_prefix          = "my-runners-dm"
   }
 }
 
 module "runner2" {
   source = "../../"
 
-  aws_region  = var.aws_region
+  vpc_id      = module.vpc.vpc_id
+  subnet_id   = element(module.vpc.public_subnets, 0)
   environment = "${var.environment}-2"
 
-  runners_use_private_address = false
+  runner_instance = {
+    name = var.runner_name
+  }
 
-  vpc_id    = module.vpc.vpc_id
-  subnet_id = element(module.vpc.public_subnets, 0)
+  runner_gitlab = {
+    url = var.gitlab_url
+  }
 
-  docker_machine_spot_price_bid = "on-demand-price"
-
-  runners_name       = var.runner_name
-  runners_gitlab_url = var.gitlab_url
-
-  gitlab_runner_registration_config = {
+  runner_gitlab_registration_config = {
     registration_token = var.registration_token
     tag_list           = "docker_spot_runner_2"
     description        = "runner public - auto"
@@ -98,11 +100,14 @@ module "runner2" {
     maximum_timeout    = "3600"
   }
 
-  cache_shared = "true"
+  runner_worker_cache = {
+    runner_worker_shared = "true"
+    create               = false
+    policy               = module.cache.policy_arn
+    bucket               = module.cache.bucket
+  }
 
-  cache_bucket = {
-    create = false
-    policy = module.cache.policy_arn
-    bucket = module.cache.bucket
+  runner_worker_docker_machine_instance = {
+    private_address_only = false
   }
 }
