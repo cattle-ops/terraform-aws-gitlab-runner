44f80b9463f90e9d16afe3770e4d2cd4a694f2b1
Matthias Kay <matthias.kay@hlag.com>
2023-09-30 16:47:02 +0200

    KAYMAN-MK-PATCH-1

--- examples/runner-multi-region/main.tf
+++ examples/runner-multi-region/main.tf
@@ -6,15 +6,19 @@ data "aws_availability_zones" "available_main_region" {
 # kics-scan ignore-line
 module "vpc_main_region" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.70"
+  version = "5.1.2"
 
   name = "vpc-${var.environment}"
   cidr = "10.1.0.0/16"
 
-  azs            = [data.aws_availability_zones.available_main_region.names[0]]
-  public_subnets = ["10.1.101.0/24"]
+  azs                     = [data.aws_availability_zones.available.names[0]]
+  private_subnets         = ["10.0.1.0/24"]
+  public_subnets          = ["10.0.101.0/24"]
+  map_public_ip_on_launch = false
 
-  map_public_ip_on_launch = "false"
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
 
   tags = {
     Environment = var.environment
@@ -73,24 +77,27 @@ module "runner_main_region" {
 # VPC Flow logs are not needed here
 # kics-scan ignore-line
 module "vpc_alternate_region" {
-  providers = {
-    aws = aws.alternate_region
-  }
-
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.70"
+  version = "5.1.2"
 
   name = "vpc-${var.environment}"
   cidr = "10.1.0.0/16"
 
   # Have to construct the zone names here because the data source always uses the main region
   azs            = ["${var.aws_alternate_region}a", "${var.aws_alternate_region}b", "${var.aws_alternate_region}c"]
-  public_subnets = ["10.1.101.0/24"]
+  private_subnets         = ["10.0.1.0/24"]
+  public_subnets          = ["10.0.101.0/24"]
+  map_public_ip_on_launch = false
 
-  map_public_ip_on_launch = "false"
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     Environment = var.environment
+  }
+
+  providers = {
+    aws = aws.alternate_region
   }
 }
 
