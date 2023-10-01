03e9f753461c692bd6ac96b8c34c58539f2e5d6c
Matthias Kay <matthias.kay@hlag.com>
2023-10-01 09:06:53 +0200

    Update versions.tf

--- examples/runner-certificates/versions.tf
+++ examples/runner-certificates/versions.tf
@@ -4,23 +4,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.0.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2"
+      version = "2.0.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = "3.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3"
+      version = "3.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "3.0.0"
     }
   }
 }
