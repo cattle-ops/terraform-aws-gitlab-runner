023971a58ff815b33332ac14f47e56e369e245a8
Matthias Kay <matthias.kay@hlag.com>
2023-11-09 10:20:41 +0100

    moved by accident

--- modules/terminate-agent-hook/iam.tf
+++ modules/terminate-agent-hook/iam.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
 # ----------------------------------------------------------------------------
 # Terminate Instances - IAM Resources
 # ----------------------------------------------------------------------------
