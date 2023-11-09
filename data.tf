04fb5d48acc487a0b3216b9fb5bd1b98d677d9ae
Matthias Kay <matthias.kay@hlag.com>
2023-11-09 10:13:35 +0100

    remove duplicate data sources

--- data.tf
+++ data.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
+
 data "aws_partition" "current" {}
+
 data "aws_region" "current" {}
 
 data "aws_subnet" "runners" {
--- modules/terminate-agent-hook/iam.tf
+++ modules/terminate-agent-hook/iam.tf
@@ -1,7 +1,3 @@
-data "aws_partition" "current" {}
-data "aws_caller_identity" "this" {}
-data "aws_region" "this" {}
-
 # ----------------------------------------------------------------------------
 # Terminate Instances - IAM Resources
 # ----------------------------------------------------------------------------
