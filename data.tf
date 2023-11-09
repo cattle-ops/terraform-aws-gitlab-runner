afefe5a3137c5d3da9369a38d4eb41decb3d0199
Matthias Kay <matthias.kay@hlag.com>
2023-11-09 10:17:03 +0100

    put all data sources in one file

--- data.tf
+++ data.tf
@@ -11,3 +11,33 @@ data "aws_subnet" "runners" {
 data "aws_availability_zone" "runners" {
   name = data.aws_subnet.runners.availability_zone
 }
+
+data "aws_ami" "runner" {
+  most_recent = "true"
+
+  dynamic "filter" {
+    for_each = var.runner_ami_filter
+    content {
+      name   = filter.key
+      values = filter.value
+    }
+  }
+
+  owners = var.runner_ami_owners
+}
+
+data "aws_ami" "docker-machine" {
+  count = var.runner_worker.type == "docker+machine" ? 1 : 0
+
+  most_recent = "true"
+
+  dynamic "filter" {
+    for_each = var.runner_worker_docker_machine_ami_filter
+    content {
+      name   = filter.key
+      values = filter.value
+    }
+  }
+
+  owners = var.runner_worker_docker_machine_ami_owners
+}
