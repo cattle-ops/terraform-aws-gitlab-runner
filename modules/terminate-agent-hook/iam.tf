ef222306e73df308cc043b290f2d1a9de828b4d1
Matthias Kay <matthias.kay@hlag.com>
2023-11-09 10:23:21 +0100

    rename resource, copy&paste error

--- modules/terminate-agent-hook/iam.tf
+++ modules/terminate-agent-hook/iam.tf
@@ -1,8 +1,8 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "this" {}
 
 data "aws_partition" "current" {}
 
-data "aws_region" "current" {}
+data "aws_region" "this" {}
 
 # ----------------------------------------------------------------------------
 # Terminate Instances - IAM Resources
