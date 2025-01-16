0522d0cceaa059296d469eb80cda7f1e74b20da0
Yevgen Karlashov <methate@gmail.com>
2025-01-16 16:59:06 +0100

    Fix docker_autoscaler_internal_traffic
    Signed-off-by: Yevgen Karlashov <methate@gmail.com>

--- docker_autoscaler.tf
+++ docker_autoscaler.tf
@@ -43,8 +43,8 @@ resource "aws_vpc_security_group_ingress_rule" "docker_autoscaler_internal_traff
   count = var.runner_worker.type == "docker-autoscaler" ? 1 : 0
 
   security_group_id            = aws_security_group.docker_autoscaler[0].id
-  from_port                    = 0
-  to_port                      = 0
+  from_port                    = -1
+  to_port                      = -1
   ip_protocol                  = "-1"
   description                  = "Allow ALL Ingress traffic between Runner Manager and Docker-autoscaler workers security group"
   referenced_security_group_id = aws_security_group.runner.id
