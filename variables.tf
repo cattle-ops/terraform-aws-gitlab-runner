e510ab6fe8f273d04fd01ba60e413ffed5df6ac8
Matthias Kay <matthias.kay@hlag.com>
2025-01-30 09:25:21 +0100

    format code

--- variables.tf
+++ variables.tf
@@ -415,7 +415,7 @@ variable "runner_worker" {
     type                  = optional(string, "docker+machine")
     # false positive, use_private_key is not a secret
     # kics-scan ignore-line
-    use_private_key       = optional(bool, false)
+    use_private_key = optional(bool, false)
   })
   default = {}
 
