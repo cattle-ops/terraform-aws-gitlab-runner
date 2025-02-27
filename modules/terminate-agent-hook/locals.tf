4d8458a158dcae287b06eef0f222d876d1f61f40
Matthias Kay <matthias.kay@hlag.com>
2025-02-27 10:45:27 +0100

    fix variable name

--- modules/terminate-agent-hook/locals.tf
+++ modules/terminate-agent-hook/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  lambda_handler = var.lambda_handler != null ? var.lambda_handler : "lambda_function.handler"
+  original_lambda_handler = "lambda_function.handler"
+  lambda_handler = var.lambda_handler != null ? var.lambda_handler : local.original_lambda_handler
 
-  replaced_environment_variables = { for key, value in var.environment_variables : key => replace(value, "{HANDLER}", local.lambda_handler) }
+  replaced_environment_variables = { for key, value in var.environment_variables : key => replace(value, "{HANDLER}", local.original_lambda_handler) }
 }
