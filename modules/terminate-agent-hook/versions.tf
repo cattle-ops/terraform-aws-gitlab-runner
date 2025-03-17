1e2c285f21e99817fdece34b49ea8dbaf073c652
Vincent Lee <5388077+vincentclee@users.noreply.github.com>
2025-03-17 19:34:46 -0400

    chore: update the terminate-agent-hook lambda to Python 3.13 with Arm/Graviton2 (#1266)
    ## Description
    
    This pull request updates the `terminate-agent-hook` Lambda function to
    use Python 3.13 and the Arm64 architecture (Graviton2). Lambda functions
    powered by Graviton2 are designed to deliver up to 19% better
    performance at 20% lower cost, improving both efficiency and
    cost-effectiveness.
    
    The change also updates the required AWS provider version to `>= 5.76`
    to ensure compatibility with the latest AWS resources and services.
    
    ---------
    
    Co-authored-by: Matthias Kay <github@matthiaskay.de>

--- modules/terminate-agent-hook/versions.tf
+++ modules/terminate-agent-hook/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws = {
-      version = ">= 4.0.0"
+      version = ">= 5.76"
       source  = "hashicorp/aws"
     }
   }
--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.26"
+      version = ">= 5.76"
     }
     local = {
       source  = "hashicorp/local"
