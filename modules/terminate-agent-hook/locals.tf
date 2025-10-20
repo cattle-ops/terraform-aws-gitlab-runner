57c17afe5228840ea03530548f81629c2fc505f2
Sebastian Melchior <801781+smelchior@users.noreply.github.com>
2025-10-20 22:14:44 +0200

    fix: correct default terminate lambda handler value (#1347)

--- modules/terminate-agent-hook/locals.tf
+++ modules/terminate-agent-hook/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  original_lambda_handler = "lambda_function.handler"
+  original_lambda_handler = "terminate_runners.handler"
   lambda_handler          = var.lambda_handler != null ? var.lambda_handler : local.original_lambda_handler
 
   replaced_environment_variables = { for key, value in var.environment_variables : key => replace(value, "{HANDLER}", local.original_lambda_handler) }
